ed0f9292eec7820ac4acb27f10f6a225ee0efd34
Matej Urbas <matej.urbas@gmail.com>
2013-03-25 11:45:26 +0100

    Updated BIND config.

--- samples/MachineConfig/src/maco/networking.py
+++ samples/MachineConfig/src/maco/networking.py
@@ -127,7 +127,8 @@ def setupHttpd(cfgScript):
 
 
 def setupBind(cfgScript):
-    createLink([dirBindConf(cfgScript), 'named.conf'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK, 0640, 'root', 'named')
+    createLink([dirBindConf(cfgScript), 'named.conf.options'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK, 0640, 'root', 'bind')
+    createLink([dirBindConf(cfgScript), 'named.conf.local'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK, 0640, 'root', 'bind')
     createLink([dirBindConf(cfgScript), '90.157.141.db'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK | UTILS_CREATE_LINK_DELETE, 0644, 'root', 'root')
     createLink([dirBindConf(cfgScript), 'urbas.si.db'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK | UTILS_CREATE_LINK_DELETE, 0644, 'root', 'root')
     createLink([dirBindConf(cfgScript), 'stanujem.si.db'], NAMED_CONF_DIR, UTILS_CREATE_LINK_HARD_LINK | UTILS_CREATE_LINK_DELETE, 0644, 'root', 'root')
